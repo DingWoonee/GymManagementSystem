777389c6af3bf902e0345271e82cf2fde986ef9b
chachacha12 <chaminwoo369@gmail.com>
2023-10-30 00:57:52 +0900

    test2

--- trainer.h
+++ trainer.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
 
-void TrainerPrompt(char* trainer_name);
+void TrainerPrompt(char* trainer_name); 
 
 void PrintMemberList(char* trainer_name);
