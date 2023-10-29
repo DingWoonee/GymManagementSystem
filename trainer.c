4ebc45b5fcc4cad82812e00850469d0516a42c36
chachacha12 <chaminwoo369@gmail.com>
2023-10-30 00:51:34 +0900

    add TraninerPrompt func menu (test commit)

--- trainer.c
+++ trainer.c
@@ -1,6 +1,11 @@
 #include "trainer.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void TrainerPrompt(char* trainer_name) {
+void TrainerPrompt(char* trainer_name) { 
+    printf("[트레이너 메뉴]\n1. 담당회원\n2. 로그아웃\n0. 종료\n선택하세요(숫자만 입력) : ");
+
+
 
 }
 
